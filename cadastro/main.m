8141d00da256e95fcdd29f7109ce9d7983823796
jairrayas <jairrayas@outlook.com>
2018-09-04 03:11:05 -0300

    Trabalho de agoritimos 2 corrigido

--- cadastro/main.m
+++ cadastro/main.m
@@ -5,14 +5,23 @@
 int main(void)
 {
     char sexo; //variavel para apenas um caractere
-    char nome[10]; //declaracao de string em C
-    printf("Digite o nome da pessoa: ");
-    //fflush(stdin); //limpa a fazer do teclado
-    //gets (nome); //le uma string
-    fgets(nome, 10, stdin);
     
-    printf("digite o sexo da pessoa: ");
+    printf("Digite a letra do sexo: ");
     fflush(stdin); //limpa a fazer do teclado
+    
     sexo = getchar(); //le um caractere
-    printf("Nome = %s, Sexo = %c\n", nome, sexo);
+    
+    if(sexo == 'M' || sexo == 'm'){
+        printf("Masculino");
+    }
+    else if(sexo == 'F' || sexo == 'f'){
+        printf("Feminino");
+    }
+    else{
+        printf("Indefinido");
+    }
+    
+    puts("\n\n");
+    
+    return 0;
 }
